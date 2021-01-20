94d792b1dde521725b9845d408738f4a95562b7b
ctithirvan <thirvansomanah@gmail.com>
2021-01-20 12:02:22 +0400

    created byr function

---
+++ Day 4/Day4TWO.py
@@ -0,0 +1,46 @@
+from typing import ValuesView
+
+
+def validatePassport(keys, values):
+    requiredKeys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]#, "cid"]
+    valid = True
+    for requiredKey in requiredKeys:
+        # print(requiredKey)
+        try:
+            keyIndex = keys.index(requiredKey)
+            print("{}: {} index: {}".format(requiredKey,values[keyIndex], keyIndex))
+        except ValueError:
+            valid = False
+    
+    return valid
+
+def validatebyr(value):
+    valid = True
+    try:
+        
+
+
+
+with open("input.txt", "r") as fReader:
+    lines = fReader.read().split("\n\n")
+
+validPassports = 0
+
+for i, line in enumerate(lines):
+    fields = []
+    keys = []
+    values = []
+    lineFields = line.split("\n")
+    for lineField in lineFields:
+        field = lineField.split(" ")
+        for f in field:
+            fields.append(f)
+            keys.append(f[:3])
+            values.append(f[4:])
+    # print("\n----" + str(i) + ":\n")
+    # print(fields)
+    # print(keys)
+    if validatePassport(keys, values):
+        validPassports += 1
+print("valid passports: " + str(validPassports) )
+# print(lines)
