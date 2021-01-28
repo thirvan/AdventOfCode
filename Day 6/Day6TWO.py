242441a58459e10be0433481f02307d94a29cab7
thirvan <thirvansomanah@gmail.com>
2021-01-28 12:22:49 +0400

    Completed Day 6

--- Day 6/Day6TWO.py
+++ Day 6/Day6TWO.py
@@ -1,12 +1,14 @@
-with open("testinput.txt") as fReader:
+with open("input.txt") as fReader:
     groups = fReader.read().split("\n\n")
 
 sumAnswers = 0
 
 for group in groups:
-    print(groups)
+    # print(groups)
+
+    yesCounter = 0
     groupLen = len(group.split("\n"))
-    print("group len: " + str(groupLen))
+    # print("group len: " + str(groupLen))
 
     #make every answer from group into a single string
     group = group.replace('\n', '')
@@ -15,7 +17,16 @@ for group in groups:
     for answer in group:
         #add letter in dict or increment if it already exists
         answersDict[answer] = answersDict.setdefault(answer, 0) + 1
-    print(answersDict)
-    print("...")
+    # print(answersDict)
+    
+    for letter in answersDict:
+        # print("letter: " + letter, end = " - ")
+        # print("num of yes in group: " + str(answersDict[letter]))
+        if answersDict[letter] == groupLen:
+            yesCounter += 1
+            # print("everyone answered " + letter)
+    
+    sumAnswers += yesCounter
+    # print("...")
 
-# print("The sum of the counts is " + str(sumAnswers))
+print("The sum of the counts is " + str(sumAnswers))
